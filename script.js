6dd314c9e1e5df3671051b26f6a581f14cec8689
Yahya Rahmandoost <39901482+YoyoRahman@users.noreply.github.com>
2024-11-08 06:51:51 +0330

    finish html and create css

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+
+  lucide.createIcons();
+
+function togglePassword(){
+    const password = document.getElementById('password')
+    const eyeIcon = document.querySelector(".eye-icon")
+
+    if(password.type === 'password'){
+        password.type = 'text'
+        eyeIcon.setAttribute('data.lucide','eye-off')
+    } else {
+        password.type ='password'
+        eyeIcon.setAttribute('data.lucide','eye')
+    }
+    lucide.createIcons()
+}
